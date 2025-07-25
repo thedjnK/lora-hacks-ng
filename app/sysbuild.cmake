c30e80db25de4f998f466af6d83958346cdbff45
Jamie McCrae <spam@helper3000.net>
2025-07-25 17:22:17 +0100

    Add combined hex file

--- app/sysbuild.cmake
+++ app/sysbuild.cmake
@@ -9,6 +9,7 @@ ExternalZephyrProject_Add(
   SOURCE_DIR ${APP_DIR}/remote
   BOARD bl54l15_breakout/nrf54l15/cpuflpr/lora/xip
   BOARD_REVISION ${BOARD_REVISION}
+  BUILD_ONLY y
 )
 
 set_property(GLOBAL APPEND PROPERTY PM_DOMAINS CPUFLPR)
@@ -19,3 +20,23 @@ set(CPUFLPR_PM_DOMAIN_DYNAMIC_PARTITION remote CACHE INTERNAL "")
 # Add dependency so that the remote image is built/flashed first
 sysbuild_add_dependencies(CONFIGURE ${DEFAULT_IMAGE} remote)
 sysbuild_add_dependencies(FLASH ${DEFAULT_IMAGE} remote)
+
+# Generated combined hex file
+add_custom_command(
+  OUTPUT
+    ${CMAKE_BINARY_DIR}/combined.hex
+  COMMAND
+    ${PYTHON_EXECUTABLE} ${ZEPHYR_BASE}/scripts/build/mergehex.py
+    -o ${CMAKE_BINARY_DIR}/combined.hex
+    ${CMAKE_BINARY_DIR}/merged.hex ${CMAKE_BINARY_DIR}/merged_CPUFLPR.hex
+  DEPENDS
+    ${DEFAULT_IMAGE}_extra_byproducts
+    remote_extra_byproducts
+)
+
+add_custom_target(
+  combined_hex
+  ALL
+  DEPENDS
+    ${CMAKE_BINARY_DIR}/combined.hex
+)
