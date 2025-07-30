2ee670a67d0a6a81512cd3ea6edb963fbb3fa55e
Jamie McCrae <spam@helper3000.net>
2025-07-30 16:33:15 +0100

    Add in settings support

--- app/src/ipc_settings.c
+++ app/src/ipc_settings.c
@@ -11,6 +11,10 @@
 #include <zephyr/logging/log.h>
 #include "ipc_endpoint.h"
 
+#if defined(CONFIG_IPC_SETTINGS_SERVER)
+#include <zephyr/settings/settings.h>
+#endif
+
 LOG_MODULE_REGISTER(ipc_settings, 4);
 
 struct ipc_setting_save_data {
@@ -78,10 +82,13 @@ static struct ipc_group ipc_group_load = {
 static int ipc_setting_callback_save(const uint8_t *message, uint16_t size, void *user_data)
 {
 	int rc;
+	struct ipc_setting_save_data *setting = (struct ipc_setting_save_data *)message;
 	struct ipc_setting_save_response_data data;
 
-LOG_ERR("abc");
-data.rc = 34;
+	rc = settings_runtime_set(setting->setting, &setting->setting[setting->name_size], setting->value_size);
+
+LOG_ERR("abc: %d for %s", rc, setting->setting);
+data.rc = rc;
 
 	rc = ipc_send_message(IPC_OPCODE_SETTINGS_SAVE, sizeof(data), (uint8_t *)&data);
 
@@ -91,16 +98,21 @@ data.rc = 34;
 static int ipc_setting_callback_load(const uint8_t *message, uint16_t size, void *user_data)
 {
 	int rc;
+	struct ipc_setting_load_data *setting = (struct ipc_setting_load_data *)message;
 	struct ipc_setting_load_response_data *data;
 
-	data = (struct ipc_setting_load_response_data *)malloc(8);
+	data = (struct ipc_setting_load_response_data *)malloc(16);
 
-data->rc = 56;
+	rc = settings_runtime_get(setting->name, data->setting, setting->max_value_size);
 
-	rc = ipc_send_message(IPC_OPCODE_SETTINGS_LOAD, sizeof(8), (uint8_t *)data);
+LOG_ERR("def: %d for %s", rc, setting->name);
+data->rc = rc;
+
+	data->value_size = (rc >= 0 ? rc : 0);
+
+	rc = ipc_send_message(IPC_OPCODE_SETTINGS_LOAD, 16, (uint8_t *)data);
 	free(data);
 
-LOG_ERR("def");
 	return rc;
 }
 #endif
@@ -124,8 +136,15 @@ static int ipc_setting_callback_load(const uint8_t *message, uint16_t size, void
 {
 	struct ipc_setting_load_response_data *data = (struct ipc_setting_load_response_data *)message;
 	ipc_settings_data.rc = data->rc;
-//	ipc_settings_data.loader_pointer = value;
-//	ipc_settings_data.loader_size = max_value_size;
+
+//do null check on ipc_settings_data.load_pointer ? do size check on ipc_settings_data.load_size ?
+
+LOG_HEXDUMP_ERR(data->setting, data->rc, "teh");
+
+	memcpy(ipc_settings_data.load_pointer, data->setting, data->value_size);
+
+	ipc_settings_data.load_pointer = NULL;
+	ipc_settings_data.load_size = 0;
 LOG_ERR("def: %d", data->rc);
 
 	k_sem_give(&ipc_settings_data.done);
@@ -137,8 +156,8 @@ int ipc_setting_save(uint8_t *name, uint8_t *value, uint8_t value_size)
 {
 	int rc;
 	struct ipc_setting_save_data *data;
-	uint8_t name_size = strlen(name);
-	uint8_t total_size = sizeof(struct ipc_setting_save_data) - sizeof(uint8_t *) + name_size + value_size;
+	uint8_t name_size = strlen(name) + 1;
+	uint8_t total_size = sizeof(struct ipc_setting_save_data) + name_size + value_size;
 
 	rc = k_sem_take(&ipc_settings_data.busy, K_FOREVER);
 
@@ -171,15 +190,15 @@ int ipc_setting_load(uint8_t *name, uint8_t *value, uint8_t max_value_size)
 {
 	int rc;
 	struct ipc_setting_load_data *data;
-	uint8_t name_size = strlen(name);
-	uint8_t total_size = sizeof(struct ipc_setting_load_data) - sizeof(uint8_t *) + name_size + 1;
+	uint8_t name_size = strlen(name) + 1;
+	uint8_t total_size = sizeof(struct ipc_setting_load_data) + name_size;
 
 	rc = k_sem_take(&ipc_settings_data.busy, K_FOREVER);
 
 	data = (struct ipc_setting_load_data *)malloc(total_size);
 	data->name_size = name_size;
 	data->max_value_size = max_value_size;
-	memcpy(data->name, name, (name_size + 1));
+	memcpy(data->name, name, name_size);
 	ipc_settings_data.load_pointer = value;
 	ipc_settings_data.load_size = max_value_size;
 
